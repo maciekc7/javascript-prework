541dc280507e34b30682b9d333393b1f8fdbf2a9
maciekc7 <ciulkinmaciek7@gmail.com>
2021-10-12 10:52:27 +0200

    add js math random

--- js/script.js
+++ js/script.js
@@ -15,3 +15,23 @@ printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wy
 var playerMove;
 playerMove = 'papier';
 printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
+
+var randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+printMessage('Wylosowana liczba to: ' + randomNumber);
+
+
+
+function getRandomIntInclusive(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+      }
+
+printMessage('Wylosowana liczba z przedzialu od 11 do 19 to ' +  getRandomIntInclusive(11,19))
+
+
+
+var newRandomNumber;
+newRandomNumber = Math.floor(Math.random() * 9 + 10 + 1);
+printMessage('Nowa wylosowana liczba to: ' + newRandomNumber);
